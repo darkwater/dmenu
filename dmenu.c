af749962f0d815403dd94f009daf7afcebfe9af5
Sam Lakerveld <darkwater124@gmail.com>
2016-12-05 23:40:32 +0100

    Changed -w to dmenu window width (centered on monitor)

--- dmenu.c
+++ dmenu.c
@@ -36,6 +36,7 @@ struct item {
 static char text[BUFSIZ] = "";
 static char *embed;
 static int mw, mh;
+static unsigned int dmw; /* dmenu width */
 static int inputw = 0, promptw;
 static int lrpad; /* sum of left and right padding */
 static size_t cursor;
@@ -590,9 +591,9 @@ setup(void)
 				if (INTERSECT(x, y, 1, 1, info[i]))
 					break;
 
-		x = info[i].x_org;
+		mw = (dmw > 0) ? dmw : info[i].width;
+		x = info[i].x_org + (info[i].width - mw) / 2;
 		y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
-		mw = info[i].width;
 		XFree(info);
 	} else
 #endif
@@ -600,9 +601,9 @@ setup(void)
 		if (!XGetWindowAttributes(dpy, parentwin, &wa))
 			die("could not get embedding window attributes: 0x%lx",
 			    parentwin);
-		x = 0;
+		mw = (dmw > 0) ? dmw : wa.width;
+		x = (wa.width - mw) / 2;
 		y = topbar ? 0 : wa.height - mh;
-		mw = wa.width;
 	}
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 	inputw = MIN(inputw, mw/3);
@@ -639,7 +640,7 @@ static void
 usage(void)
 {
 	fputs("usage: dmenu [-bfiv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
-	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
+	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w width]\n", stderr);
 	exit(1);
 }
 
@@ -680,8 +681,8 @@ main(int argc, char *argv[])
 			colors[SchemeSel][ColBg] = argv[++i];
 		else if (!strcmp(argv[i], "-sf"))  /* selected foreground color */
 			colors[SchemeSel][ColFg] = argv[++i];
-		else if (!strcmp(argv[i], "-w"))   /* embedding window id */
-			embed = argv[++i];
+		else if (!strcmp(argv[i], "-w"))   /* dmenu width */
+			dmw = atoi(argv[++i]);
 		else
 			usage();
 
